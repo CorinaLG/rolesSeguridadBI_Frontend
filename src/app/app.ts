e5f88f3d58e413ccb5c62db470f771260eb3203a
CorinaLG <corina.colque@logmark.com.bo>
2025-12-11 18:25:07 -0400

    Adding navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+
+// src/app/app.ts
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] // opcional
 })
-export class App {
-  protected readonly title = signal('RolesSeguridadBI_Frontend');
-}
+export class AppComponent {}
+
